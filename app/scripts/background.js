9809d2cc2765dd928e2dd6da77dd4958d234f944
BSick7 <brad.sickles@gmail.com>
2014-11-10 15:46:37 -0500

    Passing message with inspecting tabId.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,13 +5,7 @@ chrome.runtime.onConnect.addListener(function (port) {
   port.onMessage.addListener(function (message) {
     console.log('Received message from panel (background.js): ' + message);
     // Request a tab for sending needed information
-    chrome.tabs.query({'active': true,'currentWindow': true}, function (tabs) {
-      // Send message to content script
-      if (tabs && tabs[0]) {
-        console.log('Sending message to content script on tab ' + tabs[0].id + ' (background.js)');
-        chrome.tabs.sendMessage(tabs[0].id, message);
-      }
-    });
+    chrome.tabs.sendMessage(message.tabId, message);
   });
   // Post back to Devtools from content
   chrome.runtime.onMessage.addListener(function (message, sender) {
--- app/scripts/page/controller.js
+++ app/scripts/page/controller.js
@@ -1,12 +1,14 @@
 myApp.controller("PageController", function ($scope, pageInfoService) {
 	var port = chrome.runtime.connect({name: "devtool"});
+
+  var tabId = chrome.devtools.inspectedWindow.tabId;
 	$scope.onclick = function(name) {
-		port.postMessage({key: "tab", message: name})
+		port.postMessage({key: "tab", message: name, tabId: tabId});
 	}
 	$scope.sayHi = function() {
-		port.postMessage({key: "panel", message: "hi"});
+		port.postMessage({key: "panel", message: "hi", tabId: tabId});
 	}
 	$scope.sayHiToName = function(name) {
-		port.postMessage({key: "panel", message: name});
+		port.postMessage({key: "panel", message: name, tabId: tabId});
 	}
 });
